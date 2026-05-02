d30d6e4f18ba4ce33a429034fbfb5a095a3f02ec
叶子多 <yexixi2333@gmail.com>
2026-05-03 02:02:12 +0800

    睡了

--- Day8/排序.py
+++ Day8/排序.py
@@ -2,6 +2,21 @@
 # 2026年05月03日01时35分43秒
 # yexixi2333@gmail.com
 
-my_dict={'name':'yezi','age':25}
-print(type(my_dict))
-print(my_dict)
+import random
+class Sort:
+    def __init__(self, n):
+        """
+        n是排序的数的数量
+        :param n:
+        """
+        self.len=n
+        self.arr = [0] * n
+        self.random_data()
+
+    def random_data(self):
+        for i in range(self.len):
+            self.arr[i]=random.randint(0,99)
+
+if __name__ == '__main__':
+    my_sort=Sort(10)
+    print(my_sort.arr)
